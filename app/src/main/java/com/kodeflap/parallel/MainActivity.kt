35954aaa358d6e8cb6ecd1b6693db681fc3a5c4d
Pooja shaji <poojashaji321@gmail.com>
2022-11-17 23:43:23 +0530

    Implementing circular progress in activity page

--- app/src/main/java/com/kodeflap/parallel/MainActivity.kt
+++ app/src/main/java/com/kodeflap/parallel/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.kodeflap.parallel
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.kodeflap.parallel.composables.CircularProgressBar
 import com.kodeflap.parallel.ui.theme.ParallelTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,7 +40,16 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ParallelTheme {
-        Greeting("Android")
-    }
+    CircularProgressBar(
+        modifier = Modifier
+            .size(250.dp)
+            .background(Color.LightGray),
+        radius = 250f ,
+        initialValue = 10,
+        primaryColor = R.color.black,
+        secondaryColor = R.color.holo_blue_bright,
+        onPositionChange = {
+
+        }
+    )
 }
